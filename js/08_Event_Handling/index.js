79c26a53b2071e45c8e3300297583159971e6ded
jharry444 <jharrison1@qa.com>
2025-01-17 14:29:30 +0000

    extra examples

--- js/08_Event_Handling/index.js
+++ js/08_Event_Handling/index.js
@@ -17,3 +17,20 @@ function logTyping(event) {
 
     // console.log("Value:", demoInput.value);
 }
+
+const counterInput = document.getElementById("counterInput");
+
+function incrementCounter() {
+    counterInput.value = parseInt(counterInput.value) + 1;
+    // .value is a string so if you use + you'll end up appending 1 to the end of the value
+    // rather than increasing it
+}
+
+const fancyCounterInput = document.getElementById("fancyCounterInput");
+function changeCounter(change) {
+    if (change === 'R') {
+        fancyCounterInput.value = 0;
+    } else {
+        fancyCounterInput.value = parseInt(fancyCounterInput.value) + change;
+    }
+}
