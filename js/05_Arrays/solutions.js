b119cd5a2e8fa800b4e63acd8b8c62369c59502e
jharry444 <jharrison1@qa.com>
2025-01-17 09:56:18 +0000

    solutions

--- js/05_Arrays/solutions.js
+++ js/05_Arrays/solutions.js
@@ -1,3 +1,47 @@
+// 💡 
+// Create an array that will hold 10 integer values, populate the array with values, 
+// then call and output each element.
+
+const nums = [];
+
+nums[0] = 21;
+nums[1] = 123;
+nums[2] = 23;
+nums[3] = 43;
+nums[4] = 25;
+nums[5] = 453;
+nums[6] = 24;
+nums[7] = 563;
+nums[8] = 64;
+nums[9] = 63;
+
+console.log("Nums:", nums);
+
+
+// Create a for loop that populates an integer array with values, outputting them at each iteration.
+
+const nums2 = [];
+
+for (let i = 0; i < 10; i = i + 1) {
+    nums2[i] = Math.round(Math.random() * 10);
+}
+
+console.log("Nums2:", nums2);
+
+
+// Then create another loop that iterates through the array, changing the values at each point 
+// to equal itself times 10, outputting them at each iteration.
+
+for (let i = 0; i < nums2.length; i = i + 1) {
+    nums2[i] = nums2[i] * 10;
+
+    console.log("I:", nums2[i]);
+
+}
+
+// const means you cant replace an array/object but you CAN change it
+
+
 // Extension
 
 // 1.
---
+++ js/06_Objects/solutions.js
@@ -0,0 +1,8 @@
+
+
+const me = {
+    name: "Jordan",
+    age: 30,
+    specialism: "Software",
+    hobbies: ['Books', 'Games', 'Being a muppet']
+}
